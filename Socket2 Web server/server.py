65b50f706d8a9bcb7ff00c46d7df866bb35d7071
Kenneth Munk <contact.me@kenmunk.com>
2022-10-27 23:33:22 -0700

    all requirements have now been met
    time to go and learn how to do this in an easily multi-threaded language

--- Socket2 Web server/server.py
+++ Socket2 Web server/server.py
@@ -15,34 +15,47 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 #Fill in start
 
 #binding to local 
-tcpSocket.bind(('',tcpPort))
+serverSocket.bind(('',tcpPort))
+
+print("attempting launch")
 #Fill in end
 
 while True:
-    tcpSocket.listen(1)
-    print('The server is probably ready to receive')
-    print 'Ready to serve...'
-    connectionSocket, addr = tcpSocket.accept()
-    try:
-        message = connectionSocket.recv(1024)
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = #Fill in start #Fill in end
-        #Send one HTTP header line into socket
-        #Fill in start
+	serverSocket.listen(1)
+	print('The server is probably ready to receive')
+	print 'Ready to serve...'
+	connectionSocket, addr = serverSocket.accept()
+	try:
+		message = connectionSocket.recv(1024)
+		print (str(message))
+		filename = message.split()[1]
+		fileContents = open(filename[1:])
+		#reading the file content of the desired file
+		outputdata = fileContents.read()
+		fileContents.close()
+		print(str(outputdata))
+		#Send one HTTP header line into socket
+		#Fill in start
+		connectionSocket.send("HTTP/1.0 200 OK\r\n\r\n")
+		#Fill in end
+		#Send the content of the requested file to the client
+		for i in range(0, len(outputdata)):
+			connectionSocket.send(outputdata[i])
+		connectionSocket.close()
 
-        #Fill in end
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i])
-            connectionSocket.close()
-    except IOError:
-        #Send response message for file not found
-        #Fill in start
+	except IOError:
 
-        #Fill in end
-        #Close client socket
-        #Fill in start
+		#Send response message for file not found
+		#Fill in start
+		connectionSocket.send("404 Not Found")
 
-        #Fill in end
+
+		#Fill in end
+		#Close client socket
+		#Fill in start
+		connectionSocket.close()
+		#Fill in end
+
 serverSocket.close()
+
+print("launch end")
